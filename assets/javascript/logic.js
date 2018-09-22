0c9730bf9e59c5f690b5429242a2e039a8cbd82a
Kyle Rosenmeyer <kylerosenmeyer@gmail.com>
2018-09-22 19:25:23 -0400

    revamp sept 2018

---
+++ assets/javascript/logic.js
@@ -0,0 +1,11 @@
+
+
+ScrollReveal().reveal(".img-fluid, .desc, .sub-content, .form-group", {
+    delay: 1000,
+    duration: 4000
+})
+
+ScrollReveal().reveal(".container-title", {
+    delay: 0,
+    duration: 4000
+})
