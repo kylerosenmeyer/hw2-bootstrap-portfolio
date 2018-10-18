01bf8b4a0ffa9c3ad54d22a0b6aaf19da37c2ee8
Kyle Rosenmeyer <kylerosenmeyer@gmail.com>
2018-10-17 23:48:12 -0400

    portfolio update

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,4 +1,6 @@
-
+$(document).ready( function() {
+    $(".thankYou").fadeOut(0)
+})
 
 ScrollReveal().reveal(".img-fluid, .desc, .sub-content, .form-group, .btn-primary", {
     delay: 500,
@@ -38,6 +40,41 @@ if ( window.innerHeight > window.innerWidth ) {
         afterReveal: lineExtend
     })
 }
+
+//This is the firebase section
+
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDcxBNuS4YiXb8oTasQO5SbTGaNNzahGj8",
+    authDomain: "kr-portfolio-4775e.firebaseapp.com",
+    databaseURL: "https://kr-portfolio-4775e.firebaseio.com",
+    projectId: "kr-portfolio-4775e",
+    storageBucket: "kr-portfolio-4775e.appspot.com",
+    messagingSenderId: "410469895355"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database()
+
+$("#contactSubmit").click( function() {
+
+    event.preventDefault()
+
+    var name = $("#inputName").val().trim(),
+        email = $("#inputEmail").val().trim(),
+        message= $("#textArea").val().trim()
+
+    database.ref("users/" + name).set({
+        name: name,
+        email: email,
+        message: message
+    })
+
+    $(".contactForm").fadeOut(2000)
+    $(".thankYou").delay(2000).fadeIn(2000)
+})
+
 
 
 
